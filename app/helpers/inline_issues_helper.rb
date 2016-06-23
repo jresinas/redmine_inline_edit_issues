67ba5d373eb38e1a633c13d0767485394d1a29f6
jresinas <jresinas@emergya.com>
2016-06-23 10:46:38 +0200

    Fix error when select custom fields

--- app/helpers/inline_issues_helper.rb
+++ app/helpers/inline_issues_helper.rb
@@ -142,8 +142,8 @@ module InlineIssuesHelper
 
     tag_options = {:id => field_id, :class => "#{custom_field.field_format}_cf"}
 
-    field_format = Redmine::CustomFieldFormat.find_by_name(custom_field.field_format)
-    case field_format.try(:edit_as)
+    field_format = Redmine::FieldFormat.find(custom_field.field_format)
+    case custom_field.field_format
     when "date"
       text_field_tag(field_name, custom_value.value, tag_options.merge(:size => 10)) +
       calendar_for(field_id)
